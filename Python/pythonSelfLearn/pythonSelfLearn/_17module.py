b2cf071f348f993316c1d69a2b2a9bc8ecc9617d
edmund4work <edmund4work@users.noreply.github.com>
2024-07-21 17:26:56 +0800

    1st draft

---
+++ Python/pythonSelfLearn/pythonSelfLearn/_17module.py
@@ -0,0 +1,9 @@
+#get other page function.
+
+import _08function
+import token
+import sys
+
+a.calculater(1,2)
+
+print(sys.path)
---
+++ Python/pythonSelfLearn/pythonSelfLearn/_19apraticesQuestion.py
@@ -0,0 +1,4 @@
+class Questions01 :
+    def __init__(self, description, answer):
+        self.description = description
+        self.answer = answer
